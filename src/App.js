d908a95301b6b82a764994160f639dc1be228f07
e-a-o <kodama@Belldandy.local>
2019-03-28 22:23:21 -0500

    troubleshooting and header added

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+      <h1>React application for personal learning experience</h1>
         <header className="App-header">
+          
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
